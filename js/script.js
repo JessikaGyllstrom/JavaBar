764dea010cffead8e802fc07da1d670bc08ea30a
Jessika <72019869+JessikaGyllstrom@users.noreply.github.com>
2022-03-30 22:49:48 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+    cartItem.classList.remove('active');
+}
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () => {
+    cartItem.classList.toggle('active');
+
+}
+window.onscroll = () => {
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+}
