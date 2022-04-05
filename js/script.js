9f36471844d38e591804e6968abe9d1f8413965a
Jessika <72019869+JessikaGyllstrom@users.noreply.github.com>
2022-04-05 12:37:42 +0200

    styled cart and menu at smaller screens

--- js/script.js
+++ js/script.js
@@ -1,16 +1,35 @@
 let navbar = document.querySelector('.navbar');
 
+
+
+
+function myFunction() {
+    let cartItem = document.querySelector('.cart-items-container');
+
+    cartItem.classList.toggle('active');
+}
+
+/*
 document.querySelector('#menu-btn').onclick = () => {
     navbar.classList.toggle('active');
     cartItem.classList.remove('active');
-}
+}*/
+/*
 let cartItem = document.querySelector('.cart-items-container');
 
-document.querySelector('#cart-btn').onclick = () => {
+    document.querySelector('#cart-btn').onclick = () => {
     cartItem.classList.toggle('active');
 
-}
+}*/
 window.onscroll = () => {
     navbar.classList.remove('active');
     cartItem.classList.remove('active');
 }
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
